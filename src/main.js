8cf4e1d4ccbeffa1925f11648c2bf0f2d599be85
julia sheremet and Kristen Chellis <julia@>
2018-04-16 10:46:41 -0700

    add frontend logic to main.js

--- src/main.js
+++ src/main.js
@@ -2,5 +2,26 @@ import './styles.css';
 import { Entry } from './journal'
 
 $(document).ready(function(){
-  $("#journal-form")
-})
+  $("#journal-form").submit(function(e) {
+    e.prevent.default();
+
+    var title = $("#title").val();
+    var body = $("#body").val();
+    var newEntry = new Entry (title, body);
+
+    newEntry.countWords();
+    newEntry.countVowels();
+    newEntry.countConsonants();
+    newEntry.countEight();
+    console.log(newEntry);
+
+    $("#user-output").prepend('<div class="entry">' +
+        '<h2>' + title + '</h2>' +
+        '<p class="teaser">' + teaser + ', words: ' + newEntry.words +
+          ', vowels: ' + newEntry.vowels + ', consonants: ' + newEntry.consonants + '</p>' +
+        '<p class="body">' + body + '</p>' +
+      '</div>');
+
+
+  });
+});
